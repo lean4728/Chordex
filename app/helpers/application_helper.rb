106b6b0e98a62fa3470d0cb943176553cae7c614
Ryota Kuboki <lean4728@gmail.com>
2021-02-21 19:26:34 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = 'Chordex'
+    if page_title.empty?
+      base_title + ' | コード進行から曲を検索'
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
